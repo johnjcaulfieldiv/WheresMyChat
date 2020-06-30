7d7d7c3b89b16898eeddb50718bb2ffc38ae0d7b
johnjcaulfieldiv <johnjcaulfieldiv@gmail.com>
2020-06-30 12:59:21 -0700

    changed ClientInformation class to package private

--- src/com/WMC/Client/ClientInformation.java
+++ src/com/WMC/Client/ClientInformation.java
@@ -1,18 +1,18 @@
 package com.WMC.Client;
 
-public class ClientInformation {
+class ClientInformation {
 
-	public String displayName;
-	public String serverAddress;
-	public String serverPort;
+	String displayName;
+	String serverAddress;
+	String serverPort;
 
-	public ClientInformation() {
+	ClientInformation() {
 		this.displayName = "";
 		this.serverAddress = "";
 		this.serverPort = "";
 	}	
 	
-	public ClientInformation(String displayName, String serverAddress, String serverPort) {
+	ClientInformation(String displayName, String serverAddress, String serverPort) {
 		this.displayName = displayName;
 		this.serverAddress = serverAddress;
 		this.serverPort = serverPort;
