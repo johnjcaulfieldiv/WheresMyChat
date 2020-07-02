d9775b0d6c8a940001ea73cab5376c039e963d62
johnjcaulfieldiv <johnjcaulfieldiv@gmail.com>
2020-07-01 18:44:39 -0700

    ColorShceme persists via serialization

--- src/com/WMC/Client/ClientChatWindow.java
+++ src/com/WMC/Client/ClientChatWindow.java
@@ -2,6 +2,7 @@ package com.WMC.Client;
 
 import java.awt.EventQueue;
 
+import javax.swing.JColorChooser;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -16,12 +17,28 @@ import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.URL;
+
 import javax.swing.JScrollPane;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class ClientChatWindow extends JFrame {
 	private static final long serialVersionUID = 1L;
 	
 	private static final String HELP_STRING = "Commands: /h[elp], /quit, /exit";
+	
+	private static final String COLOR_FILENAME = "/res/clientChatWindowColorScheme";
 	
 	private JPanel contentPane;
 	private JTextArea messageTextArea;
@@ -29,14 +46,22 @@ public class ClientChatWindow extends JFrame {
 	private JScrollPane scrollPane;
 	private JScrollPane scrollPane_1;
 	
+	private ColorScheme colorScheme;
+	
 	private ClientInformation clientInfo;
 	
 	private boolean shiftPressed = false;
+	private JMenuBar menuBar;
+	private JMenu fileMenu;
+	private JMenuItem fileMenuItem_Exit;
+	private JMenu viewMenu;
+	private JMenuItem viewMenuItem_Colors;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
+					System.out.println("For Debugging purposes only. Run ClientApplication's main method in production");
 					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 					ClientChatWindow frame = new ClientChatWindow(new ClientInformation("DummyName", "127.0.0.1", "8507"));
 					frame.setVisible(true);
@@ -51,10 +76,45 @@ public class ClientChatWindow extends JFrame {
 	public ClientChatWindow(ClientInformation clientInfo) {
 		this.clientInfo = clientInfo;
 		
+		this.colorScheme = deserializeColorSchemeFromFile(COLOR_FILENAME);
+		if (colorScheme == null)
+			colorScheme = new ColorScheme();
+		
 		setTitle("Where's My Chat, JC?");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(800, 600);
-		setLocationRelativeTo(null);
+		setLocationRelativeTo(null);		
+		this.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent windowEvent) {
+				closeWindow();
+			}
+		});
+		
+		menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		fileMenu = new JMenu("File");
+		menuBar.add(fileMenu);
+		
+		fileMenuItem_Exit = new JMenuItem("Exit");
+		fileMenuItem_Exit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				closeWindow();
+			}
+		});
+		fileMenu.add(fileMenuItem_Exit);
+		
+		viewMenu = new JMenu("View");
+		menuBar.add(viewMenu);
+		
+		viewMenuItem_Colors = new JMenuItem("Change Colors");
+		viewMenuItem_Colors.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setColorSchemeWithDialog();
+			}
+		});
+		viewMenu.add(viewMenuItem_Colors);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -99,14 +159,14 @@ public class ClientChatWindow extends JFrame {
 			
 			@Override
 			public void keyReleased(KeyEvent e) {
-				if (e.getKeyCode() == KeyEvent.VK_ENTER && !shiftPressed) {
+				if (e.getKeyCode() == KeyEvent.VK_ENTER && !shiftPressed) {					
 					String message = messageTextArea.getText();
 					messageTextArea.setText("");					
 					
 					if (message.replace("\n", "").equals(""))
 						return;
 					
-					if (message.startsWith("/"))
+					if (message.trim().startsWith("/"))
 						handleCommand(message);
 					else					
 						sendUserMessage(message);
@@ -122,27 +182,47 @@ public class ClientChatWindow extends JFrame {
 			}
 		});
 		
+		setColors();
+		
 		String welcomeMessage = WMCUtil.getTimeStamp() + "\n" + 
 				clientInfo.getServerAddress() + ":" + clientInfo.getServerPort() + "\n" +
 				"Welcome to chat, " + clientInfo.getDisplayName() + "!\n\n";
 		chatTextArea.setText(welcomeMessage);
 	}	
 	
+	private void setColors() {
+		this.setBackground(colorScheme.getBackgroundColor());
+		chatTextArea.setBackground(colorScheme.getForegroundColor());
+		chatTextArea.setForeground(colorScheme.getTextColor());
+		messageTextArea.setBackground(colorScheme.getForegroundColor());
+		messageTextArea.setForeground(colorScheme.getTextColor());
+		messageTextArea.setCaretColor(colorScheme.getTextColor());
+	}
+	
+	private void setColorSchemeWithDialog() {
+		colorScheme.setForegroundColor(JColorChooser.showDialog(null, "Select chat background color", colorScheme.getBackgroundColor()));
+		colorScheme.setTextColor(JColorChooser.showDialog(null, "Select text color", colorScheme.getBackgroundColor()));
+		setColors();
+	}
+	
 	public void setMessageFocus() {
 		messageTextArea.requestFocusInWindow();
 	}
 	
 	public void handleCommand(String cmd) {
-		cmd = cmd.substring(1).trim();
+		cmd = cmd.trim().substring(1);
 		
 		if (cmd.equals("quit") || cmd.equals("exit")) {
-			this.dispose();
+			closeWindow();
 		}
 		else if (cmd.equals("h") || cmd.equals("help")) {
 			printHelp();
 		}
 		else if (cmd.equals("secret") || cmd.equals("easteregg")) {
 			systemMessage("You have discovered a secret command. Well played.");
+		}
+		else if (cmd.startsWith("color")) {
+			setColorSchemeWithDialog();
 		}
 		else {
 			systemMessage("Invalid command - " + cmd);
@@ -161,5 +241,51 @@ public class ClientChatWindow extends JFrame {
 	
 	public void systemMessage(String msg) {
 		chatTextArea.setText(chatTextArea.getText() + "[SYSTEM]: " + msg + "\n");
+	}
+	
+	public void closeWindow() {
+		this.serializeColorSchemeAndWriteToFile(COLOR_FILENAME);
+		this.dispose();
+	}
+	
+	private void serializeColorSchemeAndWriteToFile(String filename) {
+
+		URL url = ClientChatWindow.class.getResource(filename);
+		
+		try { 
+            FileOutputStream file = new FileOutputStream(url.getPath()); 
+            ObjectOutputStream out = new ObjectOutputStream(file); 
+            
+            out.writeObject(colorScheme); 
+  
+            out.close(); 
+            file.close();
+            
+            System.out.println("Successfully wrote colorScheme to file");
+        } catch (IOException e) { 
+            e.printStackTrace();
+        } 
+	}
+	
+	private ColorScheme deserializeColorSchemeFromFile(String filename) {
+
+		URL url = ClientChatWindow.class.getResource(filename);
+		
+		try {
+	        FileInputStream file = new FileInputStream(url.getPath());
+	        ObjectInputStream in = new ObjectInputStream (file); 
+
+	        ColorScheme cs = (ColorScheme) in.readObject(); 
+	
+	        in.close(); 
+	        file.close();
+	        
+            System.out.println("Successfully read colorScheme from file");
+	        
+	        return cs;
+	    }  catch (Exception e) { 
+	    	e.printStackTrace();
+	    	return null;
+	    }
 	}
 }
